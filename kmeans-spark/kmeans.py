56891661e2c600c4c571bd634596264ebb3ba3ec
davide-coccomini <davidecoccomini@gmail.com>
2020-06-02 19:06:39 +0100

    Remove useless files

--- kmeans-spark/kmeans.py
+++ kmeans-spark/kmeans.py
@@ -1,62 +1,108 @@
-from __future__ import print_function
-
 import sys
-
+import math
 import numpy as np
-from pyspark.sql import SparkSession
+from operator import add
+from pyspark import SparkContext
 
+convergedCentroids = 0
+centroids = []
+new_centroids = []
+iterations = 0
+maxIterations = 100  
 
-def parseVector(line):
-    return np.array([float(x) for x in line.split(' ')])
-
-
-def closestPoint(p, centers):
+def calculateDistance(p1, p2):
+    distance = 0
+    for index in range(len(p1)):
+        distance += (p1[index] - p2[index])**2
+    
+    return math.sqrt(distance)
+            
+def selectCentroid(point, centroids):
     bestIndex = 0
     closest = float("+inf")
-    for i in range(len(centers)):
-        tempDist = np.sum((p - centers[i]) ** 2)
+    for centroid in centroids:
+        tempDist = calculateDistance(point, centroid[1])
         if tempDist < closest:
             closest = tempDist
-            bestIndex = i
+            bestIndex = centroid[0]
     return bestIndex
 
 
+def partialSum(p1, p2):
+    coordinates_sum = list( map(add, p1[0], p2[0]))
+    points_number = p1[1] + p2[1]
+    p = [coordinates_sum, points_number]
+    return p
+
+
 if __name__ == "__main__":
-
+   
     if len(sys.argv) != 4:
-        print("Usage: kmeans <file> <k> <convergeDist>", file=sys.stderr)
+        print("Usage: kmeans <k> <threshold> <file>", file=sys.stderr)
         sys.exit(-1)
+    
+    k = int(sys.argv[1])
+    threshold = float(sys.argv[2])
+    filename = sys.argv[3]
+    
+    master = "local"
+    sc = SparkContext(master, "kmeans1")
+    
+    lines = sc.textFile(filename)
+    
+    ##CENTROIDS CONVERSION
+    tmp = [line.split(",") for line in lines.takeSample(False, k)]
+    
+    for index, centroid in enumerate(tmp):
+        centroids += [[index, [float(string) for string in centroid]]]
+    
+    
+    ##POINTS CONVERSION
+    points_rdd = lines.map(lambda line: [[float(string) for string in line.split(',')], 1])
 
-    print("""WARN: This is a naive implementation of KMeans Clustering and is given
-       as an example! Please refer to examples/src/main/python/ml/kmeans_example.py for an
-       example on how to use ML's KMeans implementation.""", file=sys.stderr)
+    while(maxIterations > iterations):
+        iterations += 1
+        #MAP
+        mapped_rdd = points_rdd.keyBy(lambda point : selectCentroid(point[0], centroids))
 
-    spark = SparkSession\
-        .builder\
-        .appName("PythonKMeans")\
-        .getOrCreate()
+        #REDUCE
+        reduced_rdd = mapped_rdd.reduceByKey(lambda p1, p2 : partialSum(p1, p2))
 
-    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
-    data = lines.map(parseVector).cache()
-    K = int(sys.argv[2])
-    convergeDist = float(sys.argv[3])
+        reduced_points = reduced_rdd.collect()
 
-    kPoints = data.takeSample(False, K, 1)
-    tempDist = 1.0
+        #print(reduced_points)
 
-    while tempDist > convergeDist:
-        closest = data.map(
-            lambda p: (closestPoint(p, kPoints), (p, 1)))
-        pointStats = closest.reduceByKey(
-            lambda p1_c1, p2_c2: (p1_c1[0] + p2_c2[0], p1_c1[1] + p2_c2[1]))
-        newPoints = pointStats.map(
-            lambda st: (st[0], st[1][0] / st[1][1])).collect()
+        new_centroids = []
 
-        tempDist = sum(np.sum((kPoints[iK] - p) ** 2) for (iK, p) in newPoints)
+        for index, reduced_point in enumerate(reduced_points):
+            converted_point = list(reduced_point)
+            centroid_index = converted_point[0]
+            centroid_coordinates = np.array(converted_point[1][0])/converted_point[1][1]
+            new_centroid = [centroid_index, centroid_coordinates]
+            new_centroids.append(new_centroid)
 
-        for (iK, p) in newPoints:
-            kPoints[iK] = p
-
-    print("Final centers: " + str(kPoints))
-
-    spark.stop()
+        convergedCentroids = 0
+        for index, centroid in enumerate(centroids):
+            distance = calculateDistance(centroid[1], new_centroids[index][1])
+            
+            if distance < threshold:
+                convergedCentroids+=1
+        
+        centroids = new_centroids
+        
+        percentage = len(centroids) * 80 / 100
+        
+        if convergedCentroids > percentage:
+            print("Centroids converged")
+            break
+            
+    
+    print(new_centroids)
+    sc.stop()
+    
+    
+    
+    
+    
+    
+    
